fb5e2d9444819191ce42e0c5a688b90911f39ced
KrebsGeorge <77137087+KrebsGeorge@users.noreply.github.com>
2025-10-15 06:53:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 base64 -d config > config.json
 UUID=${UUID:-'4fef2818-d13b-4f33-be07-0ff9e00e16a4'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/'}
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" config.json
 sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/nginx/nginx.conf
 
